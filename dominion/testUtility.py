3efef77bbb22e435cbc7049c0456abe8e10a4e8a
Zach Reed <zacwreed@gmail.com>
2020-01-18 15:31:08 -0600

    refactor code

---
+++ dominion/testUtility.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 15:42:42 2019
+
+@author: reedz
+"""
+
+import Dominion
+import random
+from collections import defaultdict
+
+def GetBoxes(nV):
+    #Define box
+    box = {}
+    box["Woodcutter"]=[Dominion.Woodcutter()]*10
+    box["Smithy"]=[Dominion.Smithy()]*10
+    box["Laboratory"]=[Dominion.Laboratory()]*10
+    box["Village"]=[Dominion.Village()]*10
+    box["Festival"]=[Dominion.Festival()]*10
+    box["Market"]=[Dominion.Market()]*10
+    box["Chancellor"]=[Dominion.Chancellor()]*10
+    box["Workshop"]=[Dominion.Workshop()]*10
+    box["Moneylender"]=[Dominion.Moneylender()]*10
+    box["Chapel"]=[Dominion.Chapel()]*10
+    box["Cellar"]=[Dominion.Cellar()]*10
+    box["Remodel"]=[Dominion.Remodel()]*10
+    box["Adventurer"]=[Dominion.Adventurer()]*10
+    box["Feast"]=[Dominion.Feast()]*10
+    box["Mine"]=[Dominion.Mine()]*10
+    box["Library"]=[Dominion.Library()]*10
+    box["Gardens"]=[Dominion.Gardens()]*nV
+    box["Moat"]=[Dominion.Moat()]*10
+    box["Council Room"]=[Dominion.Council_Room()]*10
+    box["Witch"]=[Dominion.Witch()]*10
+    box["Bureaucrat"]=[Dominion.Bureaucrat()]*10
+    box["Militia"]=[Dominion.Militia()]*10
+    box["Spy"]=[Dominion.Spy()]*10
+    box["Thief"]=[Dominion.Thief()]*10
+    box["Throne Room"]=[Dominion.Throne_Room()]*10
+    return box
+
+def GetSupplyOrder(nV):
+    supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
+                3:['Silver','Chancellor','Village','Woodcutter','Workshop'],
+                4:['Gardens','Bureaucrat','Feast','Militia','Moneylender','Remodel','Smithy','Spy','Thief','Throne Room'],
+                5:['Duchy','Market','Council Room','Festival','Laboratory','Library','Mine','Witch'],
+                6:['Gold','Adventurer'],8:['Province']}
+    return supply_order
+
+def GetSupply(nV):
+    player_names = GetPlayerNames(None)
+    box = GetBoxes(None) 
+    #number of curses and victory cards
+    if len(player_names)>2:
+        nV=12
+    else:
+        nV=8
+    nC = -10 + 10 * len(player_names)
+    #Pick 10 cards from box to be in the supply.
+    boxlist = [k for k in box]
+    random.shuffle(boxlist)
+    random10 = boxlist[:10]
+    supply = defaultdict(list,[(k,box[k]) for k in random10])
+
+
+    #The supply always has these cards
+    supply["Copper"]=[Dominion.Copper()]*(60-len(player_names)*7)
+    supply["Silver"]=[Dominion.Silver()]*40
+    supply["Gold"]=[Dominion.Gold()]*30
+    supply["Estate"]=[Dominion.Estate()]*nV
+    supply["Duchy"]=[Dominion.Duchy()]*nV
+    supply["Province"]=[Dominion.Province()]*nV
+    supply["Curse"]=[Dominion.Curse()]*nC
+
+    return supply
+
+def GetPlayerNames(nV):
+    return ["Annie","*Ben","*Carla"]
+
+def InitTrash(nV):
+    return []
